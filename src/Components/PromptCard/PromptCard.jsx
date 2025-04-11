3cb6b8cf3d2b6aa496e423f4b5c2c63123056345
PorkyJames <kwanjko17@gmail.com>
2025-04-11 00:19:42 -0700

    Green and Red depending on correctly typed element

--- src/Components/PromptCard/PromptCard.jsx
+++ src/Components/PromptCard/PromptCard.jsx
@@ -8,20 +8,60 @@ import { useState } from "react";
 
 const PromptCard = () => {
 
-    const [prompt, setPrompt] = useState(faker.word.words({ count: 200 }));
+    const [prompt, setPrompt] = useState(faker.word.words({ count: 150 }));
+    const [typedInput, setTypedInput] = useState("");
+    
     
     const refreshPrompt = () => {
-        const newPrompt = faker.word.words({ count: 200 });
+        const newPrompt = faker.word.words({ count: 150 });
         setPrompt(newPrompt);
     }
 
     return (
         <>
-            <div class="prompt">
-                <p>{prompt}</p>
+
+            <div class="prompt-card">
+                <p>
+                    {prompt.split('').map((char, idx) => {
+                        let className = '';
+                        if (idx < typedInput.length) {
+                            className = char === typedInput[idx] ? 'correct' : 'incorrect';
+                        } else if (idx === typedInput.length) {
+                            className = 'cursor'; 
+                        }
+                        
+                        return (
+                            <span className={className}>
+                                {char}
+                            </span>
+                        );
+                    })}
+                </p>
+
+                <div class="typing-info">
+                    <Button variant="primary" onClick={refreshPrompt}>Restart</Button>
+
+                    <div class="wpm">
+                        <p>WPM: </p>
+                    </div>
+
+                    <div class="timeLeft">
+                        <p>Time: </p>
+                    </div>
+
+                    <div class="Mistakes">
+                        <p>Mistakes: </p>
+                    </div>
+                </div>
             </div>
 
-            <Button variant="primary" onClick={refreshPrompt}>Restart</Button>
+            <input
+                type='text'
+                class="input"
+                value={typedInput}
+                onChange={(e) => setTypedInput(e.target.value)}
+                autoFocus
+            />
         </>
     )
 }
