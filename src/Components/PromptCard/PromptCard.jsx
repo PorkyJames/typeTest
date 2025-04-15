d2aa84c4fc086816d9b7bc3053f134d127456355
PorkyJames <kwanjko17@gmail.com>
2025-04-14 17:13:16 -0700

    Started working on Countdown

--- src/Components/PromptCard/PromptCard.jsx
+++ src/Components/PromptCard/PromptCard.jsx
@@ -10,11 +10,22 @@ const PromptCard = () => {
 
     const [prompt, setPrompt] = useState(faker.word.words({ count: 150 }));
     const [typedInput, setTypedInput] = useState("");
-    
+    const [timeLeft, setTimeLeft] = useState(30);
     
     const refreshPrompt = () => {
         const newPrompt = faker.word.words({ count: 150 });
         setPrompt(newPrompt);
+    }
+
+    //! Need to create a countdown timer from 30 seconds down to 0 
+    //! as soon as the user starts typing. Need to use the useState above
+
+    const countdown = () => {
+        if (timeLeft > 0) {
+            setTimeout(() => {
+                setTimeLeft(timeLeft - 1);
+            }, 1000);
+        }
     }
 
     return (
@@ -22,11 +33,15 @@ const PromptCard = () => {
 
             <div class="prompt-card">
                 <p>
-                    {prompt.split('').map((char, idx) => {
+                    {prompt.split('').map((char, index) => {
                         let className = '';
-                        if (idx < typedInput.length) {
-                            className = char === typedInput[idx] ? 'correct' : 'incorrect';
-                        } else if (idx === typedInput.length) {
+                        if (index < typedInput.length) {
+                            if (char === typedInput[index]) {
+                                className = 'correct';
+                            } else {
+                                className = 'incorrect';
+                            }
+                        } else if (index === typedInput.length) {
                             className = 'cursor'; 
                         }
                         
@@ -46,7 +61,7 @@ const PromptCard = () => {
                     </div>
 
                     <div class="timeLeft">
-                        <p>Time: </p>
+                        <p>Time: {typingTimer}</p>
                     </div>
 
                     <div class="Mistakes">
@@ -55,6 +70,7 @@ const PromptCard = () => {
                 </div>
             </div>
 
+            {/* There is an issue with autoFocus not working */}
             <input
                 type='text'
                 class="input"
