f8a9e5476f4805740ea14cd979c561c390afce31
PorkyJames <kwanjko17@gmail.com>
2025-05-27 20:47:26 -0700

    Blurred prompt on no focus

--- src/components/PromptCard/PromptCard.tsx
+++ src/components/PromptCard/PromptCard.tsx
@@ -3,7 +3,7 @@
 //! Imports
 import "./PromptCard.css"
 import { Button } from "react-aria-components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Timer from "../Timer/Timer";
 
 type HomePageProps = {
@@ -17,9 +17,14 @@ export default function PromptCard({setTestComplete, setCharsTyped, setCorrectCo
     const [prompt, setPrompt] = useState<string>("")
     const [userInput, setUserInput] = useState<string>("")
     const [startedTyping, setStartedTyping] = useState<boolean>(false)
+    const [isFocused, setIsFocused] = useState<boolean>(false)
+
+    const inputRef = useRef<HTMLInputElement | null>(null)
 
     useEffect(() => {
+        handleFocus()
         handlePromptRestart()
+        setIsFocused(true)
     }, [])
 
     const handlePromptRestart = async () => {
@@ -27,6 +32,12 @@ export default function PromptCard({setTestComplete, setCharsTyped, setCorrectCo
         const data = await res.json()
         setPrompt(data.prompt)
         setUserInput("")
+        setIsFocused(true)
+    }
+
+    const handleFocus = () => {
+        setIsFocused(true);
+        return inputRef.current?.focus()
     }
 
     //! Grab our prompt, split it, and compare to an input that's invisible
@@ -48,36 +59,49 @@ export default function PromptCard({setTestComplete, setCharsTyped, setCorrectCo
     return (
         <>
 
-            <div className="prompt-wrapper">
+            <div className="prompt-wrapper"
+                onClick = { () => {
+                        handleFocus()
+                        setIsFocused(true)
+                    }  
+                }
+            >
 
-                {startedTyping ? <Timer setTestComplete={setTestComplete}/> : <></>}
+                {startedTyping ? 
+                <Timer setTestComplete={setTestComplete}/> 
+                : 
+                <></>
+                }
 
                 <div className="prompt-card">
-                    <h2>
+                    <h2 className={!isFocused ? "blurred" : ""}>
                         {promptSplit(prompt)}
                     </h2>
+
+                    <div className="input-div">
+                        <input 
+                            className="input"
+                            value={userInput}
+                            onChange={(e) => {
+                                setUserInput(e.target.value)
+                                setCharsTyped(userInput.length)
+                                let correct = 0;
+                                for (let i = 0; i < userInput.length; i++) {
+                                    if (userInput[i] === prompt[i]) correct++;
+                                }
+                                setCorrectCount(correct);
+                                if (!startedTyping) {
+                                    setStartedTyping(true)
+                                }
+                            }}
+                            ref={inputRef}
+                            onBlur={() => setIsFocused(false)}
+                        >
+                        </input>
+                    </div>
+                    
                 </div>
 
-                <div className="input-div">
-                    <input 
-                        className="input"
-                        value={userInput}
-                        onChange={(e) => {
-                            setUserInput(e.target.value)
-                            setCharsTyped(userInput.length)
-                            let correct = 0;
-                            for (let i = 0; i < userInput.length; i++) {
-                                if (userInput[i] === prompt[i]) correct++;
-                            }
-                            setCorrectCount(correct);
-                            if (!startedTyping) {
-                                setStartedTyping(true)
-                            }
-                        }}
-                        autoFocus
-                    >
-                    </input>
-                </div>
 
                 <div className="button-div">
                     <Button onClick={handlePromptRestart}>Restart</Button>
