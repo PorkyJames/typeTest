386c3e7d9894235c3ca65dffec93e4634bfec861
PorkyJames <kwanjko17@gmail.com>
2025-05-29 23:01:08 -0700

    Started new component TRPopUp

--- src/components/TestResults/TestResults.tsx
+++ src/components/TestResults/TestResults.tsx
@@ -1,3 +1,5 @@
+import "../TestResults/TestResults.css"
+
 import { useHomeContext } from "@/lib/useHomeContext"
 
 export default function TestResults() {
@@ -20,9 +22,67 @@ export default function TestResults() {
 
     return (
         <>
-            <h1>WPM: {calculateWPM(charsTyped)}</h1>
-            <h1>Accuracy: {calculateAcc(correctCount).toFixed(1)}</h1>
-            <button onClick={setFalse}>Press to Restart Test</button>
+
+            <div className="tr-top-wrapper">
+                <div className="wpm-result">
+                    WPM: {calculateWPM(charsTyped)}
+                </div>
+
+                <div className="acc-result">
+                    Acc: {calculateAcc(correctCount).toFixed(1)}
+                </div>
+            </div>
+
+            {/* Add a keyboard that shows the amount of errors on each keystroke */}
+            {/* Need to make it DRY */}
+            <div className="keyboard-wrapper">
+                <div className="keyb-top">
+                    <div className="keyb-key-top">q</div>
+                    <div className="keyb-key-top">w</div>
+                    <div className="keyb-key-top">e</div>
+                    <div className="keyb-key-top">r</div>
+                    <div className="keyb-key-top">t</div>
+                    <div className="keyb-key-top">y</div>
+                    <div className="keyb-key-top">u</div>
+                    <div className="keyb-key-top">i</div>
+                    <div className="keyb-key-top">o</div>
+                    <div className="keyb-key-top">p</div>
+                    <div className="keyb-key-top">[</div>
+                    <div className="keyb-key-top">]</div>
+                </div>
+
+                <div className="keyb-mid">
+                    <div className="keyb-key-mid">a</div>
+                    <div className="keyb-key-mid">s</div>
+                    <div className="keyb-key-mid">d</div>
+                    <div className="keyb-key-mid">f</div>
+                    <div className="keyb-key-mid">g</div>
+                    <div className="keyb-key-mid">h</div>
+                    <div className="keyb-key-mid">j</div>
+                    <div className="keyb-key-mid">k</div>
+                    <div className="keyb-key-mid">l</div>
+                    <div className="keyb-key-mid">;</div>
+                    <div className="keyb-key-mid">'</div>
+                </div>
+
+                <div className="keyb-bot">
+                    <div className="keyb-key-bot">z</div>
+                    <div className="keyb-key-bot">x</div>
+                    <div className="keyb-key-bot">c</div>
+                    <div className="keyb-key-bot">v</div>
+                    <div className="keyb-key-bot">b</div>
+                    <div className="keyb-key-bot">n</div>
+                    <div className="keyb-key-bot">m</div>
+                    <div className="keyb-key-bot">,</div>
+                    <div className="keyb-key-bot">.</div>
+                    <div className="keyb-key-bot">/</div>
+                </div>
+
+            </div>
+
+            <div className="restart-btn">
+                <button onClick={setFalse}>Press to Restart Test</button>
+            </div>
         </>
     )
 }
