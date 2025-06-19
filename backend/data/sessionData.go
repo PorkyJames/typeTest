5f0532f8556f2a1cc3c36f36385389afabca694e
PorkyJames <kwanjko17@gmail.com>
2025-06-19 09:27:54 -0700

    Added routes directory

--- backend/data/sessionData.go
+++ backend/data/sessionData.go
@@ -9,4 +9,5 @@ var DummySession = []models.Session{
 	{Jwt_id: 1, User_id: 1, Issued_at: time.Now(), Expires_at: time.Now()},
 	{Jwt_id: 2, User_id: 2, Issued_at: time.Now(), Expires_at: time.Now()},
 	{Jwt_id: 3, User_id: 3, Issued_at: time.Now(), Expires_at: time.Now()},
+	{Jwt_id: 4, User_id: 4, Issued_at: time.Now(), Expires_at: time.Now()},
 }
